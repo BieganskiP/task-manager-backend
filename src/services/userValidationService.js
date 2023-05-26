414df9bd51f13289198c519a32a20868591c2fa5
BieganskiP <bieganski1996@gmail.com>
2023-05-26 11:16:49 +0200

    sav

--- src/services/userValidationService.js
+++ src/services/userValidationService.js
@@ -6,7 +6,7 @@ const userValidationSchema = Joi.object({
     .min(8)
     .required()
     .pattern(
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\p{P}\p{S}])[A-Za-z\d\p{P}\p{S}]{8,}$/u
     )
     .message(
       "Password must be at least 8 characters long and contain one uppercase letter, one lowercase letter, one number, and one special symbol"
