f4bc3b8bdaa235273fda50054d1eba45396f7391
BieganskiP <bieganski1996@gmail.com>
2023-05-26 10:07:53 +0200

    save

--- src/services/userValidationService.js
+++ src/services/userValidationService.js
@@ -2,7 +2,15 @@ const Joi = require("joi");
 
 const userValidationSchema = Joi.object({
   email: Joi.string().email().required(),
-  password: Joi.string().min(6).required(),
+  password: Joi.string()
+    .min(8)
+    .required()
+    .pattern(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+    )
+    .message(
+      "Password must be at least 8 characters long and contain one uppercase letter, one lowercase letter, one number, and one special symbol"
+    ),
 });
 
 const updateSubscriptionSchema = Joi.object({
